f09139d153a753dc1caef76bfd2b7a1e20889739
rohithvarma444 <rohithvarmadatla12@gmail.com>
2025-08-17 01:14:57 +0530

    fixing bugs in request handler

--- Request-Handler/src/index.ts
+++ Request-Handler/src/index.ts
@@ -4,13 +4,11 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-
-con
 const s3 = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    endpoint: process.env.AWS_ENDPOINT,
-})
+        region: 'ap-south-1',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 const app = express();
 
@@ -19,14 +17,28 @@ app.get("/*", async (req, res) => {
     const id = host.split(".")[0];
     const filePath = req.path;
 
+    console.log(id);
+    console.log(filePath);
+
     const contents = await s3.getObject({
         Bucket: 'my-vercelapp',
         Key: `dist/${id}${filePath}`
     }).promise();
     
-    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    const type = filePath.endsWith("html") ? "text/html" : 
+                 filePath.endsWith("css") ? "text/css" : 
+                 filePath.endsWith("svg") ? "image/svg+xml" :
+                 filePath.endsWith("js") ? "application/javascript" :
+                 filePath.endsWith("json") ? "application/json" :
+                 filePath.endsWith("png") ? "image/png" :
+                 filePath.endsWith("jpg") ? "image/jpeg" :
+                 filePath.endsWith("gif") ? "image/gif" :
+                 filePath.endsWith("woff") ? "font/woff" :
+                 filePath.endsWith("woff2") ? "font/woff2" :
+                 filePath.endsWith("ttf") ? "font/ttf" :
+                 "text/plain";
+    
     res.set("Content-Type", type);
-
     res.send(contents.Body);
 })
 
